c67a01413a210fd375f069bea0b8a759bf11921b
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-24 22:28:30 +0500

    imp

--- app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Routes package for Education Center Management System
-"""
-
-# Import all routers for easy access
-from . import auth, admin, teacher, student, parent, files
-
-__all__ = ["auth", "admin", "teacher", "student", "parent", "files"]

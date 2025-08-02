58a12d22978bada16893c14f7d997dc65a9cebf2
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-08-02 18:50:20 +0500

    imp

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
-    JWT_EXPIRE_MINUTES: int = 60
+    JWT_EXPIRE_MINUTES: int = 10080
     UPLOAD_DIR: str = "uploads"
     MAX_FILE_SIZE: int = 10 * 1024 * 1024
     MAX_IMAGE_SIZE: int = 3 * 1024 * 1024
